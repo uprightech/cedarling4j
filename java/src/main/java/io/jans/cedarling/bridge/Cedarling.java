d8a20348613bd1bbab4edd7b293f12046192b49b
Rolain Djeumen <uprightech@gmail.com>
2025-07-03 06:22:59 +0100

    fix: added exception signature to cedarling object methods
    Signed-off-by: Rolain Djeumen <uprightech@gmail.com>

--- java/src/main/java/io/jans/cedarling/bridge/Cedarling.java
+++ java/src/main/java/io/jans/cedarling/bridge/Cedarling.java
@@ -11,6 +11,13 @@ import java.io.Closeable;
 import io.jans.cedarling.bridge.config.BootstrapConfiguration;
 import io.jans.cedarling.bridge.authz.*;
 
+/**
+ * Encapsulates the Cedarling engine and marshalls data between the Java and the Rust components of the library
+ *  <p>
+ *      This is the heart of the library. 
+ *      <br/>Implicitly , this class loads the rust library by calling {@link java.lang.System#loadLibrary(String)}
+ * </p>
+ */
 public class Cedarling implements Closeable {
     
     private static boolean initialized = false;
@@ -24,11 +31,19 @@ public class Cedarling implements Closeable {
         initJniCache();
     }
 
-    public Cedarling(final BootstrapConfiguration config) {
+    /**
+     * Constructs a new {@code Cedarling} instance with the specified configuration 
+     * @param config the {@code BootstrapConfiguration used to initialize the cedarling instance}
+     * @throws io.jans.cedarling.bridge.CedarlingError if the provided configuration is null or invalid
+     */
+    public Cedarling(final BootstrapConfiguration config) throws CedarlingError {
 
         createNativeCedarling(config);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void close() {
         
@@ -37,9 +52,18 @@ public class Cedarling implements Closeable {
 
     private static native void initJniCache();
 
-    public native AuthorizeResult authorize(final AuthorizeRequest request);
-    public native AuthorizeResult authorizeUnsigned(final AuthorizeRequestUnsigned request);
+    /**
+     * Evaluates an authorization request 
+     * <p>
+     *  This is the standard (token-based) interface where the Principal is extracted
+     *  from a JWT.
+     * </p>
+     * @param request
+     * @return
+     */
+    public native AuthorizeResult authorize(final AuthorizeRequest request) throws CedarlingError;
+    public native AuthorizeResult authorizeUnsigned(final AuthorizeRequestUnsigned request) throws CedarlingError;
 
-    private native void createNativeCedarling(final BootstrapConfiguration config);
+    private native void createNativeCedarling(final BootstrapConfiguration config) throws CedarlingError;
     private native void cleanupCedarling();
 }
